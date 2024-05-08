b3b75285905a7e10a1310bdd4d4b0589f9a0c70a
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-05-08 22:15:36 +0300

    update modal

--- src/js/js-modal.js
+++ src/js/js-modal.js
@@ -3,6 +3,7 @@
     openModalBtn1: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn1: document.querySelector('[data-modal-close]'),
     modal1: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn1.forEach(item =>
@@ -10,10 +11,17 @@
   );
 
   refs.closeModalBtn1.addEventListener('click', toggleModal1);
+  refs.backdrop.addEventListener('click', onBackdropClick);
 
   function toggleModal1() {
     refs.modal1.classList.toggle('is-hidden');
     document.body.classList.toggle('modal-open');
+  }
+
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      toggleModal1();
+    }
   }
 
   if ('ontouchstart' in window) {
