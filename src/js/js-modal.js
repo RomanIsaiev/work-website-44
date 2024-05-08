04e13cf7aaf92abca9db81fb8a90734bf19f1333
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-05-08 20:54:50 +0300

    added modal and scroll

---
+++ src/js/js-modal.js
@@ -0,0 +1,43 @@
+const refs = {
+  openModalBtn: document.querySelectorAll('[data-action="open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector(".js-backdrop"),
+};
+
+// refs.openModalBtn.addEventListener("click", onOpenModal);
+
+refs.openModalBtn.forEach((item) =>
+  item.addEventListener("click", onOpenModal)
+);
+
+refs.closeModalBtn.addEventListener("click", onCloseModal);
+refs.backdrop.addEventListener("click", onBackdropClick);
+
+function onOpenModal() {
+  window.addEventListener("keydown", onEscKeyPress);
+  document.body.classList.add("show-modal");
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  document.body.classList.remove("show-modal");
+}
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
+}
+
+function toggleModal() {
+  refs.modal.classList.toggle("is-hidden");
+}
